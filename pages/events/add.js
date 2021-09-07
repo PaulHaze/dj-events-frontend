3f5339f6b7a465c12962a7ace9e354ae9befe10c
PaulHaze <dev@artemist.com.au>
2021-09-08 09:39:36 +1000

    add an events folder for nested routes

---
+++ pages/events/add.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function AddEventPage() {
+  return (
+    <div>
+      <Head>
+        <title>Add An Event</title>
+      </Head>
+      <p>add event placeholder</p>
+    </div>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function EventsPage() {
+  return (
+    <div>
+      <Head>
+        <title>Events</title>
+      </Head>
+      <p>My Events Placholder</p>
+    </div>
+  );
+}
