eeaf8ea44028bfb911097637766e800c9cb6bb3c
PaulHaze <dev@artemist.com.au>
2021-09-08 10:06:18 +1000

    add metropolis font

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Head>
+        <title>About Page</title>
+      </Head>
+      <h1>About</h1>
+      <p>Find The Latest Dj Events Near You</p>
+    </div>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function EventPage() {
+  return (
+    <div>
+      <Head>
+        <title>Event Details</title>
+      </Head>
+      <p>Event Page Details</p>
+    </div>
+  );
+}
