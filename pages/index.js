9b0182aed4d27d8c6837b204489bf52a1211d807
PaulHaze <dev@artemist.com.au>
2021-09-08 13:26:57 +1000

    add link ro index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -7,14 +8,20 @@ export default function Home() {
         <title>Dj Events</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-[100vh] bg-gradient-to-br from-green-300/50 to-sky-800/50 flex items-center justify-center px-3">
-        <div className="bg-white p-xl justify-center text-center rounded-lg shadow-2xl">
-          <h1 className="bg-clip-text bg-gradient-to-b from-indigo-400 to-violet-800 text-transparent font-bold text-center">
-            Im your new Tailwind Project
+      <div className="min-h-[100vh] bg-gradient-to-br from-green-300/50 to-sky-800/50 flex items-start justify-center pt-5 px-3">
+        <div className="bg-white p-xl rounded-lg shadow-2xl text-center">
+          <h1 className="bg-clip-text bg-gradient-to-b from-indigo-400 to-violet-800 text-transparent  text-center">
+            New Tailwind Project
           </h1>
-          <p className="text-[#504e63] text-center">
-            Delete this and make something great
-          </p>
+
+          <Link href="/about">
+            <button
+              type="button"
+              className="border rounded shadow px-4 py-2 mt-10"
+            >
+              About
+            </button>
+          </Link>
         </div>
       </div>
     </div>
