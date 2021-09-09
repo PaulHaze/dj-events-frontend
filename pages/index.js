c49b772b16c2325fecbaf7b9bac2afc6089cd37e
PaulHaze <dev@artemist.com.au>
2021-09-09 12:38:29 +1000

    add grid for buttons on home page

--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,41 @@ import MainLayout from '../layouts/MainLayout';
 
 export default function Home() {
   return (
-    <MainLayout title="DJ Events">
+    <MainLayout title="Bookerly | Your one stop shop for funding, booking and managing your next party">
       <div className="container p-5 max-w-xl">
-        <h1 className="font-heading">DJ Events Listings</h1>
-        <p>
-          Your one stop shop for all the latest night lub events and parties
-        </p>
-        <div className="flex space-x-3">
+        <h1 className="font-heading">Bookerly</h1>
+        <p>Find, book and manage your next party all in one place.</p>
+        <div className="grid grid-cols-2 gap-4 sm:gap-0 sm:grid-cols-none sm:flex sm:space-x-3 mt-4">
           <Link href="/about">
             <button
               type="button"
-              className="border rounded shadow px-4 py-2 mt-10"
+              className="border rounded shadow px-4 py-2 text-sm whitespace-nowrap hover:bg-coolgray-200/50"
             >
-              About
+              Find A Venue
             </button>
           </Link>
           <Link href="/events">
             <button
               type="button"
-              className="border rounded shadow px-4 py-2 mt-10"
+              className="border rounded shadow px-4 py-2 text-sm whitespace-nowrap"
             >
-              Events
+              Venues
             </button>
           </Link>
           <Link href="/events/add">
             <button
               type="button"
-              className="border rounded shadow px-4 py-2 mt-10"
+              className="border rounded shadow px-4 py-2 text-sm whitespace-nowrap"
             >
-              Add An Event
+              Manage A Booking
+            </button>
+          </Link>
+          <Link href="/events/add">
+            <button
+              type="button"
+              className="border rounded shadow px-4 py-2 text-sm whitespace-nowrap"
+            >
+              Update Your Venue
             </button>
           </Link>
         </div>
