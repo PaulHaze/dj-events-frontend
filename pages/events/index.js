bd3967df668ecb54553ac158d12b1e07fcecfffa
PaulHaze <dev@artemist.com.au>
2021-09-14 17:09:38 +1000

    add event page and placeholder

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,37 +1,17 @@
-import Link from 'next/link';
+import { Hero, LinkButton } from '@/components/index';
 import MainLayout from '@/layouts/MainLayout';
 
 export default function EventsPage() {
   return (
     <MainLayout title="DJ Events">
+      <Hero
+        imgUrl="/images/showcase.jpg"
+        title="EVENTS"
+        subTitle="Upcoming parties near you"
+      />
       <div className="container p-5 max-w-xl">
-        <h1 className="font-heading">Events</h1>
-        <p>Latest Dj Events Near You</p>
-        <div className="flex space-x-3">
-          <Link href="/">
-            <button
-              type="button"
-              className="border rounded shadow px-4 py-2 mt-10"
-            >
-              Home
-            </button>
-          </Link>
-          <Link href="/about">
-            <button
-              type="button"
-              className="border rounded shadow px-4 py-2 mt-10"
-            >
-              About
-            </button>
-          </Link>
-          <Link href="/events/add">
-            <button
-              type="button"
-              className="border rounded shadow px-4 py-2 mt-10"
-            >
-              Add An Event
-            </button>
-          </Link>
+        <div className="flex">
+          <LinkButton href="/events/add" linkText="Add An Event" />
         </div>
         <div className="mt-10">
           <p className="text-sm font-heading text-gray-400">Version 1.0.0</p>
