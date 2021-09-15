203acd38db8cb2e08792c50d750c709561b53487
PaulHaze <dev@artemist.com.au>
2021-09-15 17:54:31 +1000

    update edit delete buttons

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,25 +1,85 @@
+/* eslint-disable max-len */
 import Link from 'next/link';
+import Image from 'next/image';
 
-import { useRouter } from 'next/router';
 import MainLayout from '@/layouts/MainLayout';
 import { LinkButton } from '@/components/index';
 
 import { API_URL } from '@/config/index';
+import { BiPencil } from 'react-icons/bi';
+import { BsTrash } from 'react-icons/bs';
 
 export default function EventPage({ event }) {
-  // const router = useRouter();
+  const handleEditEvent = () => {
+    console.log('Edit Pressed');
+  };
 
-  // const [event] = event;
-  console.log(event);
-  // const url = router.query.slug;
-  // console.log('slug from props: ', slug);
-  // console.log(router.query.slug);
+  const handleDeleteEvent = () => {
+    console.log('Edit Pressed');
+  };
+
   return (
     <MainLayout>
       <div className="container p-5 max-w-xl">
-        <h1 className="font-heading">Event Details</h1>
+        <div className="container mt-5">
+          {/* EDIT DELETE BUTTONS */}
+          <div className="flex justify-between sm:justify-end sm:space-x-3">
+            <button
+              type="button"
+              onPress={handleEditEvent}
+              className="flex text-sky-600 text-sm font-semibold mr-5 mb-2"
+            >
+              <span className="fill-current text-sky-600 mr-1">
+                <BiPencil size={16} />
+              </span>{' '}
+              Edit Event
+            </button>
+            <button
+              type="button"
+              onPress={handleDeleteEvent}
+              className="flex text-red-600 text-sm font-semibold"
+            >
+              <span className="fill-current mr-1">
+                <BsTrash size={16} />
+              </span>{' '}
+              Delete Event
+            </button>
+          </div>
+        </div>
+
+        {/* TIME */}
         <div className="">
-          <h4 className="font-bold font-heading">{event.name}</h4>
+          <h4 className="font-light font-heading text-coolgray-400">
+            {event.date} @ {event.time}
+          </h4>
+        </div>
+
+        {/* NAME */}
+        <div className="">
+          <h1 className="font-heading text-4xl md:text-6xl mt-2 w-4/5 md:w-3/4">
+            {event.name}
+          </h1>
+        </div>
+
+        {/* IMAGE */}
+
+        <Image src={event.image} width="960" height="600" />
+        {/* <div
+          className="w-full flex h-[300px] sm:h-[380px] md:h-[480px] overflow-hidden"
+          style={{
+            backgroundImage: `url(${event.image})`,
+            backgroundPosition: `center`,
+            backgroundSize: `cover`,
+            backgroundRepeat: 'no-repeat',
+          }}
+        /> */}
+
+        {/* DJS */}
+        <div className="mt-5 flex items-center">
+          <h2 className="font-heading">Performers: </h2>
+          <h5 className="text-coolgray-400 font-semibold ml-2 pt-2">
+            {event.performers}
+          </h5>
         </div>
         <div className="flex mt-10">
           <LinkButton href="/events" linkText="Back To Events" />
@@ -54,16 +114,15 @@ export async function getStaticProps({ params }) {
     },
   };
 }
+
 /* export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/events/${slug}`);
-  const evt = await res.json();
-  const [event] = evt;
-  // console.log(event);
-  // const event = events.find(e => e.slug === slug);
+  const eventst = await res.json();
+  const evt = events.find(e => e.slug === slug);
   return {
     props: {
       slug,
-      event,
+      event: evt:[0],
     },
   };
 }
